1174f9020a0155e7d8da5144b48af31a1d02bcaa
Ryder James <work.jamesr@gmail.com>
2026-03-01 18:48:23 -0700

    feat: Add camera zoom

--- scripts/pan_camera.gd
+++ scripts/pan_camera.gd
@@ -1,11 +1,21 @@
 extends Node3D
 
 
+@export_group("Zoom")
+@export var _min_zoom := 5.0
+@export var _default_zoom := 15.0
+@export var _max_zoom := 25.0
+@export var _zoom_sensitivity := 5.0
+
 var _is_panning := false
 var _camera_motion: Vector2
 
+@onready var _zoom := _default_zoom
+
 
 func _process(delta: float) -> void:
+	position.z = _zoom
+	
 	if not _is_panning:
 		return
 	
@@ -19,6 +29,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		_is_panning = true
 	elif event.is_action_released("pan"):
 		_is_panning = false
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			_zoom = max(_zoom - _zoom_sensitivity, _min_zoom)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
+			_zoom = min(_zoom + _zoom_sensitivity, _max_zoom)
 	
 	if _is_panning and event is InputEventMouseMotion:
 		_camera_motion = event.relative
